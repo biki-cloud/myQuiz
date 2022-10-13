4704fdd6c2b1f3c5eb441d735a782ea90098d58b
hibiki <hibiki123123@gmail.com>
2022-10-13 22:43:53 +0900

    create repository. first commit.

---
+++ src/main/java/com/example/myquiz/MyQuizApplication.java
@@ -0,0 +1,13 @@
+package com.example.myquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyQuizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyQuizApplication.class, args);
+    }
+
+}
