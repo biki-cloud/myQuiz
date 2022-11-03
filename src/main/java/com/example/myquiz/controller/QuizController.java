dca775f45ed6e390eab32a1a9a7d58c08d7870f4
hibiki <hibiki123123@gmail.com>
2022-11-03 10:49:10 +0900

    ホームページにhtmlStringを登録する機能を実装。

--- src/main/java/com/example/myquiz/controller/QuizController.java
+++ src/main/java/com/example/myquiz/controller/QuizController.java
@@ -5,8 +5,11 @@ import com.example.myquiz.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class QuizController {
@@ -20,6 +23,13 @@ public class QuizController {
     @GetMapping("/")
     public String home(Model model, @ModelAttribute Quiz quiz) {
         return quizService.home(model, quiz);
+    }
+
+    @PostMapping("/register")
+    public String add(@Validated @ModelAttribute Quiz quiz,
+                      BindingResult result,
+                      Model model) {
+        return quizService.register(quiz, result, model);
     }
 }
 
--- src/main/java/com/example/myquiz/service/QuizService.java
+++ src/main/java/com/example/myquiz/service/QuizService.java
@@ -4,6 +4,8 @@ import com.example.myquiz.model.Quiz;
 import com.example.myquiz.repository.QuizRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Service
@@ -16,7 +18,25 @@ public class QuizService {
 
     public String home(Model model, @ModelAttribute Quiz quiz) {
         System.out.println("homeメソッドが呼ばれました");
-        model.addAttribute("questions", repository.findAll());
+        model.addAttribute("quizContents", repository.findAll());
         return "home";
+    }
+
+    public String register(@Validated @ModelAttribute Quiz quiz,
+                      BindingResult result,
+                      Model model) {
+        System.out.println("registerメソッドが呼ばれました");
+        model.addAttribute("quizContents", repository.findAll());
+        if (result.hasErrors()) {
+            System.out.println("バインドエラーが起きました");
+            System.out.println(result.getFieldErrors()); // エラー内容を見る
+            return "home";
+        }
+        repository.save(quiz); // オブジェクトをDBに保存するJPAにあるメソッド
+        /**
+        redirectした場合は、下でreturnした後はControllerのGetMapping("/")を担当している
+        home関数が呼び出される。
+         */
+        return "redirect:/";
     }
 }
