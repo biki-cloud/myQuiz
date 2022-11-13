cda84533557bb534b882e1a8325ba216cf906b8d
hibiki <hibiki123123@gmail.com>
2022-11-13 12:34:41 +0900

    Questionクラスにガード節を追加

--- src/main/java/com/example/myquiz/quiz/Question.java
+++ src/main/java/com/example/myquiz/quiz/Question.java
@@ -5,15 +5,11 @@ import org.jsoup.nodes.Element;
 public class Question {
     final public String value;
 
-    public Question(String value) {
-        validation(value);
-        this.value = value;
-    }
-
-    private static void validation(final String text) {
-        if (text.equals("")) {
-            throw new IllegalArgumentException("問題が空です。");
+    public Question(final String value) {
+        if (value.equals("")) {
+            throw new IllegalArgumentException("問題が空です");
         }
+        this.value = value;
     }
 
     /**
@@ -23,7 +19,6 @@ public class Question {
      */
     public static Question getQuestionFromHtmlElement(final Element element) {
         final String questionString = element.getElementsByClass("qtext").text();
-        validation(questionString);
         return new Question(questionString);
     }
 }
