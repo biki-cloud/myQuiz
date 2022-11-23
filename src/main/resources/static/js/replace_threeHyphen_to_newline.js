5f1a59f4556355b43e001a08592c0e7467f29cc3
hibiki <hibiki123123@gmail.com>
2022-11-23 11:57:36 +0900

    リファクタリング

--- src/main/resources/static/js/replace_threeHyphen_to_newline.js
+++ src/main/resources/static/js/replace_threeHyphen_to_newline.js
@@ -1,14 +1,4 @@
-// console.log("load replace_threeHyphen_to_newline.js");
-// var choicesList = document.getElementsByClassName("choices");
-// console.log(choicesList);
-// for (var i = 0; i < choicesList.length; i++) {
-//     var choices = choicesList[i];
-//     choices.innerHTML = choices.innerHTML.replaceAll("---", "<br />");
-// }
-
 function replace_to_newline(element) {
-    console.log("Replace!!");
-    console.log(element);
-    console.log(element.innerHTML)
+    console.log("replace to newline");
     element.innerHTML = element.innerHTML.replaceAll("---", "<br />");
 }
